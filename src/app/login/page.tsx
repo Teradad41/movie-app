d85f25e2d4b80acd2c1245c26f4aff355ca08fc4
Kyo Terada <sitiang120@gmail.com>
2024-06-13 20:33:16 +0900

    ✨ emailログイン機能を追加

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,18 +1,14 @@
+import { SubmitButton } from "@/components/submit-button"
 import { createClient } from "@/utils/supabase/server"
 import { headers } from "next/headers"
 import Link from "next/link"
 import { redirect } from "next/navigation"
-import { SubmitButton } from "./submit-button"
 
 export const metadata = {
   title: "Login",
 }
 
-export default function Login({
-  searchParams,
-}: {
-  searchParams: { message: string }
-}) {
+export default function Login() {
   const signIn = async (formData: FormData) => {
     "use server"
 
@@ -29,7 +25,7 @@ export default function Login({
       return redirect("/login?message=Could not authenticate user")
     }
 
-    return redirect("/protected")
+    return redirect("/")
   }
 
   const signUp = async (formData: FormData) => {
@@ -49,6 +45,7 @@ export default function Login({
     })
 
     if (error) {
+      console.error(error)
       return redirect("/login?message=Could not authenticate user")
     }
 
@@ -56,70 +53,67 @@ export default function Login({
   }
 
   return (
-    <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
-      <Link
-        href="/"
-        className="absolute left-8 top-8 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm"
-      >
-        <svg
-          role={"img"}
-          aria-label={"Back"}
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="mr-2 h-4 w-4 transition-transform group-hover:-translate-x-1"
+    <div className="flex justify-center container">
+      <div className="flex flex-col py-14 md:w-1/3 gap-2">
+        <Link
+          href="/"
+          className="py-6 hover:bg-btn-background-hover flex group text-sm"
         >
-          <polyline points="15 18 9 12 15 6" />
-        </svg>{" "}
-        Back
-      </Link>
+          <svg
+            role={"img"}
+            aria-label={"Back"}
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="mr-2 h-5 w-4 transition-transform group-hover:-translate-x-1"
+          >
+            <polyline points="15 18 9 12 15 6" />
+          </svg>{" "}
+          Back
+        </Link>
 
-      <form className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground">
-        <label className="text-md" htmlFor="email">
-          Email
-        </label>
-        <input
-          className="rounded-md px-4 py-2 bg-inherit border mb-6"
-          name="email"
-          placeholder="you@example.com"
-          required
-        />
-        <label className="text-md" htmlFor="password">
-          Password
-        </label>
-        <input
-          className="rounded-md px-4 py-2 bg-inherit border mb-6"
-          type="password"
-          name="password"
-          placeholder="••••••••"
-          required
-        />
-        <SubmitButton
-          formAction={signIn}
-          className="bg-green-700 rounded-md px-4 py-2 text-foreground mb-2"
-          pendingText="Signing In..."
-        >
-          Sign In
-        </SubmitButton>
-        <SubmitButton
-          formAction={signUp}
-          className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2"
-          pendingText="Signing Up..."
-        >
-          Sign Up
-        </SubmitButton>
-        {searchParams?.message && (
-          <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
-            {searchParams.message}
-          </p>
-        )}
-      </form>
+        <form className="px-10 animate-in flex-1 flex flex-col gap-2 text-foreground">
+          <label className="text-md" htmlFor="email">
+            Email
+          </label>
+          <input
+            className="rounded-md px-4 py-2 bg-inherit border mb-6"
+            name="email"
+            placeholder="you@example.com"
+            required
+          />
+          <label className="text-md" htmlFor="password">
+            Password
+          </label>
+          <input
+            className="rounded-md px-4 py-2 bg-inherit border mb-6"
+            type="password"
+            name="password"
+            placeholder="••••••••"
+            required
+          />
+          <SubmitButton
+            formAction={signIn}
+            className="bg-gray-950 text-white rounded-md px-4 py-3 text-foreground mb-2 hover:opacity-90 hover:bg-gray-900 focus:ring-2 focus:ring-gray-800 focus:outline-none"
+            pendingText="Signing In..."
+          >
+            Sign In
+          </SubmitButton>
+          <SubmitButton
+            formAction={signUp}
+            className="border border-foreground/20 rounded-md px-4 py-2.5 text-foreground mb-2 hover:bg-foreground/5 focus:ring-2 focus:ring-foreground focus:outline-none"
+            pendingText="Signing Up..."
+          >
+            Sign Up
+          </SubmitButton>
+        </form>
+      </div>
     </div>
   )
 }
